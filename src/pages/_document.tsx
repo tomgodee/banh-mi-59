a208e4424eec9858b27be4fb03b95a9dfe2064c9
tomgodee <trungvu8295@gmail.com>
2023-04-21 20:29:10 +0700

    Add structured data

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,6 +19,27 @@ export default function Document() {
           gtag('config', 'G-SJCGC0FHMM');
         `}
       </Script>
+
+      <script type="application/ld+json">
+        {`
+          "@context": "http://schema.org/",
+          "@type": "Restaurant",
+          "name": "Banh mi 59",
+          "image": "logo.jpeg",
+          "telephone": "+84934381995",
+          "hasMenu": "https://www.banhmi59.com/menu",
+          "servesCuisine": "Vietnamese",
+          "priceRange": "$",
+          "address": {
+            "@type": "PostalAddress",
+            "postalCode": "100000",
+            "streetAddress": "59b Cua Nam"
+          },
+          "keywords": "banh mi, baguette, beef, chicken, traditional, vietnamese, cuisine, food",
+          "description": "a little store selling great banh mi",
+          "url": "https://www.banhmi59.com"
+        `}
+      </script>
       <body>
         <Main />
         <NextScript />
